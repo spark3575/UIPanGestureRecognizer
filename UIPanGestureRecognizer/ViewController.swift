cb680a0a096ae3d7abfbf2db65c66e84e26d33e6
Shin Park <s@ss-imac.frontierlocal.net>
2019-03-05 10:46:13 -0600

    Initial Commit

---
+++ UIPanGestureRecognizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIPanGestureRecognizer
+//
+//  Created by Shin Park on 3/5/19.
+//  Copyright © 2019 shindevx.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
